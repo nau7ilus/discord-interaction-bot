044ddb79e3b8351939ec6aa541ebf0eed1d6dc39
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-02-20 02:52:25 +0300

    Lib: Interactions

---
+++ application/lib/interactions.js
@@ -0,0 +1,11 @@
+({
+  CallbackTypes: {
+    Pong: 1,
+    ChannelMessageWithSource: 4,
+    DeferredChannelMessageWithSource: 5,
+    DeferredUpdateMessage: 6,
+    UpdateMessage: 7,
+    ApplicationCommandAutocompleteResult: 8,
+    Modal: 9,
+  },
+});
