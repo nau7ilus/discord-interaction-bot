ab9f30c3d1839a063c299404fda6d56ae07a00ad
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-02-20 06:17:38 +0300

    fix(applicationCommand): removed console.log

--- application/api/interactions.1/applicationCommand.js
+++ application/api/interactions.1/applicationCommand.js
@@ -4,7 +4,6 @@
     await lib.auth.checkToken(authToken);
     const interaction = await lib.interactions.parse(rawInteraction);
     const commands = api.commands.get();
-    console.log(commands);
     const command = commands.find(({ name }) => name === interaction.data.name);
     if (!command) return { type: lib.interactions.CallbackTypes.ChannelMessageWithSource, data: { content: 'Error ' } };
     return lib.commands[command.fileName].method(interaction);
