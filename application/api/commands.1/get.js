9c4efe588eab8889bb0c78397a1a652ae725d350
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-02-20 06:11:06 +0300

    fix(commads.get): show not only the name but also a filepath

--- application/api/commands.1/get.js
+++ application/api/commands.1/get.js
@@ -2,11 +2,11 @@
   access: 'public',
   method: () => {
     const commands = [];
-    for (const commandName of Object.keys(lib.commands)) {
-      if (commandName === 'parent') continue;
-      const commandData = lib.commands[commandName].data;
+    for (const fileName of Object.keys(lib.commands)) {
+      if (fileName === 'parent') continue;
+      const commandData = lib.commands[fileName].data;
       if (!commandData) continue;
-      commands.push(commandData);
+      commands.push({ ...commandData, fileName });
     }
     return commands;
   },
