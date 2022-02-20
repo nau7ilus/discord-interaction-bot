8d26c3a6774de10579ce4ef48af2e6468692bc8b
Philipp Zelinski <43653566+nieopierzony@users.noreply.github.com>
2022-02-20 04:10:30 +0300

    lib(interactions): add CommandTypes enum

--- application/lib/interactions.js
+++ application/lib/interactions.js
@@ -5,7 +5,12 @@
     DeferredChannelMessageWithSource: 5,
     DeferredUpdateMessage: 6,
     UpdateMessage: 7,
-    ApplicationCommandAutocompleteResult: 8,
+    CommandAutocompleteResult: 8,
     Modal: 9,
+  },
+  CommandTypes: {
+    ChatInput: 1,
+    User: 2,
+    Message: 3,
   },
 });
